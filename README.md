1f7862a7888e0fcbdf9847ca50ec698dddc2af43
Muhammad Aaqib Ansari <139334963+UnderratedAaqib@users.noreply.github.com>
2024-11-28 17:28:28 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# E-Commerce-Data-Rendering-App-using-React.jS
+# E-Commerce-Data-Rendering-using-React-Toolkit
+Redux Toolkit to manage state throughout your entire application in such a way so that it can be accessed by any component without following the hierarchy between the components
---
+++ SuperCoin.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const SuperCoin = ({ totalAmount }) => {
+  const [superCoins, setSuperCoins] = useState(0);
+
+  useEffect(() => {
+    if (totalAmount >= 100 && totalAmount < 200) {
+      setSuperCoins(10);
+    } else if (totalAmount >= 200 && totalAmount < 300) {
+      setSuperCoins(20);
+    } else if (totalAmount >= 300) {
+      setSuperCoins(30);
+    } else {
+      setSuperCoins(0);
+    }
+  }, [totalAmount]);
+
+  return (
+    <div className="super-coins" style={{ textAlign: 'center' }}>
+      <h2 className="super-coins-title">Super Coins</h2>
+      <p className="super-coins-info">You will earn {superCoins} super coins with this purchase.</p>
+    </div>
+  );
+};
+
+export default SuperCoin;
